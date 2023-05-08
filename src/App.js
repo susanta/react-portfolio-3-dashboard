aac7800a8b0df35eab4f70b00498d7919e40b60c
Susanta <inbox.susanta@gmail.com>
2023-05-08 09:59:56 +0530

    Added contacts scene

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
+import Contacts from './scenes/contacts';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -20,6 +21,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
             </Routes>
           </main>
         </div>
