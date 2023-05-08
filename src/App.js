f0999d6fbaa5dbfc94b2bd08f0b17007a6848a9a
Susanta <inbox.susanta@gmail.com>
2023-05-08 10:20:20 +0530

    Added invoices scene

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
 import Contacts from './scenes/contacts';
+import Invoices from './scenes/invoices';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -22,6 +23,7 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
             </Routes>
           </main>
         </div>
