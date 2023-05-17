d0715f0a61a88d8300c79e82daf3c495dbb8ac56
Susanta <inbox.susanta@gmail.com>
2023-05-17 20:37:45 +0530

    Added Barchart

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Invoices from './scenes/invoices';
 import Form from './scenes/form';
 import Calendar from './scenes/calendar';
 import FAQ from './scenes/faq';
+import Bar from './scenes/bar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -30,6 +31,7 @@ function App() {
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<FAQ />} />
+              <Route path="/bar" element={<Bar />} />
             </Routes>
           </main>
         </div>
